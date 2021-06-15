1d4860554a4204fb05142923448a2efd2f874a2b
devrshee12 <84611501+devrshee12@users.noreply.github.com>
2021-06-15 14:57:12 +0530

    Update linear_diophantine_equation.cpp

--- number theory/linear_diophantine_equation.cpp
+++ number theory/linear_diophantine_equation.cpp
@@ -4,13 +4,6 @@ using namespace std;
 #define pb push_back
 #define maxi 3E5+5
 
-//using extended euclid algorithum
-//find only positive integer solution
-//linear diophantnie equation
-
-//how to implement extended euclid algo and how to find only positive integer solution
-
-
 
 int gcd(int a, int b)
 {
@@ -49,32 +42,36 @@ void solve()
 	// cout<<x<<" "<<y<<endl;
 	// // int x1 = (x*c)/ans;
 	// // int x2 = (y*c)/ans;
-	if(c%ans != 0)
+	if(c%ans != 0)//to check whether solution is exist or not
 	{
 		cout<<"NO SOLUTION"<<endl;
 		return;
 	}
-	x *= c/ans;
-	y *= c/ans;
+	x *= c/ans;//convert ex_euclid algo into linear diophantine equation(for finding general solutions)(only finds one solution)
+	y *= c/ans;//convert ex_euclid algo into linear diophantine equation(for finding general solutions)(only finds one solution)
 	if(a<0)x = -x;
 	if(b<0)y = -y;
 	// cout<<x<<" "<<y<<endl;
 	int l = (-1)*x;
-	int t1 = (l*ans)/b;
-	int t2 = (y*ans)/a;
+	int t1 = (l*ans)/b;//checking k limits for only positive solution
+	int t2 = (y*ans)/a;//checking k limits for only positive solution
 	// cout<<t1<<" "<<t2<<endl;
 	// ll c = 0;
 	int count = 0;
+	// for positive solution lies between t1 < k < t2
+
+	//Xall = x + (k*b)/g;
+	//Yall = y - (k*a)/g;
 	for(ll i=t1+1;i<t2;i++)
 	{
-		count++;
+		count++;//to count how many positive solution does exist
 	}
 	cout<<count<<endl;
-	for(ll i=t1+1;i<t2;i++)
+	for(ll i=t1+1;i<t2;i++)// for positive solution lies between t1 < k < t2
 	{
-		int mainx = x + (i*b)/ans;
-		int mainy =y - (i*a)/ans;
-		cout<<mainx<<" "<<mainy<<endl;
+		int mainx = x + (i*b)/ans;//Xall = x + (k*b)/g;
+		int mainy =y - (i*a)/ans;//Yall = y - (k*a)/g;
+		cout<<mainx<<" "<<mainy<<endl;//to print positive solution of equation
 		// count++;
 
 	}
